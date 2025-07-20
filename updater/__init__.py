3bb2673d91325f5ca49590ddf5f41e2ba96154ff
tpersp <130591916+tpersp@users.noreply.github.com>
2025-07-20 14:44:24 +0200

    full reset again.

--- updater/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Updater package.
-
-Contains the logic used to update this application from GitHub.
-"""
-
-from .update import perform_update
-
-__all__ = ["perform_update"]
--- viewer.py
+++
@@ -1,5 +0,0 @@
-import webview
-
-if __name__ == '__main__':
-    webview.create_window('EchoView Display', 'http://localhost:8000/static/display.html', fullscreen=True)
-    webview.start()
