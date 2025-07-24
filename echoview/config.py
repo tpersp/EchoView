995d7977265cd8c7668294099440c8eacd3eb3a1
tpersp <130591916+tpersp@users.noreply.github.com>
2025-07-25 00:28:52 +0200

    Update config.py

--- echoview/config.py
+++ echoview/config.py
@@ -27,7 +27,7 @@ load_env()
 # Application Version & Paths
 # ------------------------------------------------------------
 
-APP_VERSION = "1.0.8" # 
+APP_VERSION = "1.1.0" # 
 
 VIEWER_HOME = os.environ.get("VIEWER_HOME", "/home/pi/EchoView")
 IMAGE_DIR   = os.environ.get("IMAGE_DIR", "/mnt/EchoViews")
